37656215978f80f53e4dd6b213510756a19eb97d
DESKTOP-A4SUUOT\a <zhora-safrayan.99@mail.ru>
2024-05-02 11:22:29 +0200

    added a new location pag and javascript changes

--- Projekt_Travel/js/app.js
+++ Projekt_Travel/js/app.js
@@ -7,19 +7,22 @@ let top_block = document.getElementById("top_block");
 let pics_pic = document.querySelectorAll(".item_pic");
 let pic_res = document.getElementById("pic_res");
 
+let btn_add = document.getElementById("btn_add");
+let form_m = document.getElementById("form_m");
 let m_1 = false;
-
-setInterval(function () {
-  if (m_1 === false) {
-    d_1.style.opacity = "0";
-    d_2.style.opacity = "1";
-    m_1 = true;
-  } else {
-    d_1.style.opacity = "1";
-    d_2.style.opacity = "0";
-    m_1 = false;
-  }
-}, 1000);
+if (d_1 != undefined) {
+  setInterval(function () {
+    if (m_1 === false) {
+      d_1.style.opacity = "0";
+      d_2.style.opacity = "1";
+      m_1 = true;
+    } else {
+      d_1.style.opacity = "1";
+      d_2.style.opacity = "0";
+      m_1 = false;
+    }
+  }, 1000);
+}
 
 let key_wether = "9b2134590bea1de49510f364bec32fde";
 
@@ -36,29 +39,44 @@ async function currentWeather() {
   });
 }
 currentWeather();
-
-let date = new Date();
-let year_current = date.getFullYear();
-if (copyright != null) copyright.innerHTML = `Copyright @ ${year_current}`;
-
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 300) {
-    top_block.style.display = "block";
-    top_block.style.opacity = "1";
-  } else {
-    top_block.style.display = "none";
-    top_block.style.opacity = "0";
-  }
-});
-top_block.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "instant",
+if (copyright != undefined) {
+  let date = new Date();
+  let year_current = date.getFullYear();
+  if (copyright != null) copyright.innerHTML = `Copyright @ ${year_current}`;
+}
+if (top_block != undefined) {
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 300) {
+      top_block.style.display = "block";
+      top_block.style.opacity = "1";
+    } else {
+      top_block.style.display = "none";
+      top_block.style.opacity = "0";
+    }
   });
-});
-
-pics_pic.forEach(function (image) {
-  image.addEventListener("click", function () {
-    pic_res.src = this.src;
+  top_block.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant",
+    });
   });
-});
+}
+if (pics_pic != undefined) {
+  pics_pic.forEach(function (image) {
+    image.addEventListener("click", function () {
+      pic_res.src = this.src;
+    });
+  });
+}
+
+/**обработка формы new location*/
+
+function form_load(event) {
+  event.preventDefault();
+  let city = document.getElementsByClassName("city")[0].value;
+  let country = document.getElementsByClassName("country")[0].value;
+  let date_from = document.getElementById("date_from").value;
+  let date_to = document.getElementById("date_to").value;
+  let description = document.getElementsByClassName("description")[0].value;
+  console.log(city, country, date_to, date_from, description);
+}
